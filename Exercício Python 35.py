30ebec90715068edf8ae50928f845bee2852c787
Erick Eloi <65841249+erickeloi@users.noreply.github.com>
2021-02-04 00:59:50 -0300

    Lê 3 valores e vê se elas podem formar um triangulo

---
+++ Exercício Python 35.py
@@ -0,0 +1,9 @@
+# Desafio 35
+# lê o valor de 3 retas e responde se elas podem formar um triângulo
+reta1 = float(input("Digite aqui o valor da primeira reta: "))
+reta2 = float(input("Digite aqui o valor da segunda reta: "))
+reta3 = float(input("Digite aqui o valor da terceira reta: "))
+if (reta2 + reta3) > reta1 and (reta1 + reta3) > reta2 and (reta1 + reta2) > reta3:
+    print("Dá para formar um triângulo")
+else:
+    print("Não dá para formar um triângulo")
