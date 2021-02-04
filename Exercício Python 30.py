bc53a5d7bd972014338b15b6c6b1da9384280570
Erick Eloi <65841249+erickeloi@users.noreply.github.com>
2021-02-04 00:32:19 -0300

    Lê um número e diz se ele é PAR ou ÍMPAR

---
+++ Exercício Python 30.py
@@ -0,0 +1,7 @@
+# Desafio 30
+# Lê e mostra se um numero inteiro é PAR ou ÍMPAR ( lembrando que o "Modulo %" é o resto da divisão, e o // é a divisão inteira )
+num = int(input("Digite o número inteiro: "))
+if num % 2 == 0:
+    print('Esse número é PAR !')
+else:
+    print("Esse número é ÍMPAR !")
