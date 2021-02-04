916dfb404ab0f6bf9f1e081c6bb51976627a02cf
Erick Eloi <65841249+erickeloi@users.noreply.github.com>
2021-02-04 02:13:09 -0300

    Calcula o preço de uma viagem

--- Exercício Python 31.py
+++ Exercício Python 31.py
@@ -2,7 +2,7 @@
 # lê a distância em Km e calcula o preço da passagem.
 # (0,5R$ / Km) até 200Km e 0,45 para viagens mais longas.
 distance = float(input("Digite aqui a distância a ser percorrida em Km: "))
-if distance > 200:
-    print(f"Então, o preço da passagem é {distance * 0.45}R$")
-else:
+if distance <= 200:
     print(f"Então, o preço da passagem é {distance * 0.5}R$")
+else:
+    print(f"Então, o preço da passagem é {distance * 0.45}R$")
