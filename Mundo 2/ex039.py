ca0f3724fd56f1b19405c805f69be0f7861f9e1c
Erick Eloi <65841249+erickeloi@users.noreply.github.com>
2021-02-08 17:51:41 -0300

    Alistamento Militar
    Complementando o programa

--- Mundo 2/ex039.py
+++ Mundo 2/ex039.py
@@ -4,9 +4,11 @@ from time import localtime
 year = int(localtime()[0])
 data = int(input("Digite sua data de nascimento: "))
 idade = year - data
+print(f"Então,você tem {idade} anos")
 if idade == 18:
-    print(f"Então você tem {idade} anos e está no momento certo para se alistar")
+    print(f"E, está no momento certo para se alistar")
 elif idade < 18:
-    print(f"Então ainda falta {18 - idade} anos para você se alistar")
+    print(f"E, ainda falta {18 - idade} ano(s) para você se alistar")
+    print(f"Ou seja, você vai se alistar no ano de {data + 18}")
 elif idade > 18:
-    print(f"Então seu alistamento deveria ter sido feito {idade - 18} anos atrás")
+    print(f"E, seu alistamento deveria ter sido feito {idade - 18} ano(s) atrás, no ano de {data + 18}")
