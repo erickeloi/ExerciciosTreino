4d4b15cc74604368fb63d30014ada7c08027bc44
Erick Eloi <65841249+erickeloi@users.noreply.github.com>
2021-02-08 18:20:20 -0300

    lê o valor de 3 retas e mostra informações sobre triângulos.

--- Mundo 2/ex042.py
+++ Mundo 2/ex042.py
@@ -6,7 +6,7 @@ reta3 = float(input("Digite aqui o valor da terceira reta: "))
 if (reta2 + reta3) > reta1 and (reta1 + reta3) > reta2 and (reta1 + reta2) > reta3:
     print("Dá para formar um triângulo")
     if reta1 == reta2 == reta3:
-        print("E, ele é um triângulo equilátero, pois possui todos os lados iguais de {reta1}")
+        print(f"E, ele é um triângulo equilátero, pois possui todos os lados iguais de {reta1}")
     elif reta1 != reta2 and reta1!= reta3 and reta2 != reta3:
         print("E, ele é um triângulo escaleno, pois possui todos os lados diferentes.")
     elif (reta1 == reta2 and reta1 != reta3) or (reta2 == reta3 and reta2 != reta1):
