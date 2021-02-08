89c6642ddecd29367214d8371baf8ba75cdb7a03
Erick Eloi <65841249+erickeloi@users.noreply.github.com>
2021-02-08 14:46:07 -0300

    Categoria de natação por idade

---
+++ Mundo 2/ex041.py
@@ -0,0 +1,16 @@
+# Desafio 41
+# Lê idades e mostra sua categoria de natação
+from time import localtime
+year = int(localtime()[0])
+useryear = int(input("Digite sua data de nascimento: "))
+idade = year - useryear
+if idade <= 9:
+    print("Então você está na categoria MIRIM")
+elif 10 <= idade <= 14:
+    print("Então você está na categoria INFANTIL")
+elif 15 <= idade <= 19:
+    print("Então você está na categoria JUNIOR")
+elif idade == 20:
+    print("Então você está na categoria SENIOR")
+elif idade > 20:
+    print("Então você está na categoria MASTER")
