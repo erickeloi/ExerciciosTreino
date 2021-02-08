19e104c716e9465cda42e1a7f6ba94c5e69ef1f4
Erick Eloi <65841249+erickeloi@users.noreply.github.com>
2021-02-08 17:06:40 -0300

    Aprovador de empréstimo para compra de imóvel
    Esqueci de converter anos em meses para calcular a mensalidade, corrigi agora, tudo certo!

--- Mundo 2/ex036.py
+++ Mundo 2/ex036.py
@@ -2,9 +2,12 @@
 # Programa comprador de casa
 casa = float(input("Digite o valor da casa selecionada: "))
 salario = float(input("Digite o valor do seu salário: "))
-tempo = int(input("Digite em quantos anos você pretende pagar toda a casa: "))
-if casa / tempo > 0.3 * salario:
+anos = int(input("Digite em quantos anos você pretende pagar toda a casa: "))
+meses = anos * 12
+if casa / meses > 0.3 * salario:
+    print(f"Para pagar uma casa de {casa}R$ em {anos} anos,\n"
+          f"as parcelas seriam de {casa/meses:.3f}R$ ")
     print("Emprestimo negado.")
 else:
     print("Emprestimo aprovado!")
-    print(f"Você irá pagar {(casa / tempo)}R$ mensalmente por {tempo} anos até quitar o móvel.")
+    print(f"Você irá pagar {casa / meses:.3f}R$ mensalmente por {anos} anos até quitar o móvel.")
