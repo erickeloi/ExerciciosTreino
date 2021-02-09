1d0d6e00bd02e16403097adcab491271a94e74c5
Erick Eloi <65841249+erickeloi@users.noreply.github.com>
2021-02-09 17:52:22 -0300

    Contagem regressiva fim de ano com 'for'

---
+++ Mundo 2/ex046.py
@@ -0,0 +1,8 @@
+# Desafio 46
+# Contagem Regressiva
+from time import sleep
+print("Atenção!!! 10 segundos para o final do ano!!!")
+for c in range(10, 0, -1):
+    print(c)
+    sleep(1)
+print("Roberto Carlos!!! Com você!!!")
