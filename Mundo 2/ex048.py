a7f1e354f2f46e8e72ad5b768bf1bd968b9db6de
Erick Eloi <65841249+erickeloi@users.noreply.github.com>
2021-02-10 15:18:45 -0300

    Soma dos números ímpares múltiplos de 3 entre 1 a 500
    Adicionei um contador

--- Mundo 2/ex048.py
+++ Mundo 2/ex048.py
@@ -1,8 +1,10 @@
 # Desafio 48
 # Soma dos números ímpares multiplos de 3 entre 1 a 500
 soma = 0
+contador = 0
 for num in range(1, 501):
     if num % 2 != 0 and num % 3 == 0:
         print(num)
+        contador += 1
         soma += num
-print(f"Então, a soma desses números acima é {soma}")
+print(f"Então, a soma desses {contador} números acima é {soma}")
