a2432c38817d57341209ca054855ec2763c8a4c0
Erick Eloi <65841249+erickeloi@users.noreply.github.com>
2021-02-12 11:33:54 -0300

    Lê uma frase/palavra e verifica se é palíndromo

--- Mundo 2/ex053.py
+++ Mundo 2/ex053.py
@@ -1,11 +1,11 @@
 # Desafio 53
 # Lê um texto e verifica se ele é palindromo
 textopuro = str(input("Digite o texto: ")).strip()
-textoagrupado = textopuro.split()
+textoagrupado = textopuro.upper().split()
 textoagrupado = ''.join(textopuro)
 if textoagrupado == textoagrupado[::-1]:
     print("É um palíndromo!!!")
     print(f"''{textopuro}'' é igual de trás pra frente.")
 else:
     print("Não é Palíndromo!")
-    
+
