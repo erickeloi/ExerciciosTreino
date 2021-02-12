ef2de7feee0822eb11610e6d6a46be22d609e8c4
Erick Eloi <65841249+erickeloi@users.noreply.github.com>
2021-02-12 11:45:14 -0300

    Leitor de maioridade

--- Mundo 2/ex054.py
+++ Mundo 2/ex054.py
@@ -1,11 +1,14 @@
 # Desafio 54
 # Programa que lê 7 datas de nascimento e verifica quantas atingiram a maioridade (21 anos para cima)
 from time import localtime
-soma = 0
+soma_maior = 0
+soma_menor = 0
 ano_atual = int(localtime()[0])
 for contador in range(1, 8):
     data_nascimento = int(input(f"Digite a data de nascimento do indivíduo número {contador}: "))
     print("Anotado!")
     if (ano_atual - data_nascimento) >= 21:
-        soma = soma + 1
-print(f"Então, dentre esses {contador} indivíduos, {soma} são maiores de idade (21 anos)!")
+        soma_maior = soma_maior + 1
+    else:
+        soma_menor = soma_menor + 1
+print(f"Então, dentre esses {contador} indivíduos, {soma_maior} são maiores de idade (>21 anos)!\nE {soma_menor} indivíduos são menores de idade (<21 anos)")
