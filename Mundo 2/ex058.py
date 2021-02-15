e3c124d33b3c6e1c6736d72ede6297b6fdccd1a5
Erick Eloi <65841249+erickeloi@users.noreply.github.com>
2021-02-15 15:14:23 -0300

    Jogo da adivinhação 2.0
    Debugei  a opção '69' de Desistir de adivinhar.

--- Mundo 2/ex058.py
+++ Mundo 2/ex058.py
@@ -4,18 +4,21 @@ import random
 num = random.randrange(0, 11)
 contador = 0
 advinha = 11
-while 69 != num != advinha:
+while 69 != advinha != num:
+    print(f"Digite '69' para desistir.")
     advinha = int(input("Advinhe que número eu estou pensando, é de 0 a 10: "))
     contador += 1
     if num == advinha:
         print("Acertou!!!")
+        print(f"O número que eu pensei foi o \033[33m'{num}'\033[m")
+        print(f"Você tentou já tentou \033[31m{contador}\033[m vezes")
         if contador == 1:
             print("Ganhou de primeira!!!, Parabéns!!!")
             print(f"Pensei no número {num} mesmo")
+    elif advinha == 69:
+        print("Tente novamente mais tarde então! obrigado")
     elif num != advinha:
         print("Tente Novamente!, sua cabeça é tão grande que parece a do MegaMente!!!")
-        print(f"Você já tentou {contador} vezes")
-        print(f"Digite '69' para desistir.")
+        print(f"Você já tentou \033[31m{contador}\033[m vezes")
 print("Fim de Jogo")
-print(f"O número que eu pensei foi o \033[33m'{num}'\033[m")
-print(f"Você tentou \033[31m{contador}\033[m vezes")
+
