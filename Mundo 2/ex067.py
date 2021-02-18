5a23c9ca5b25340e9f81ed9df1c0842d9ad3bc30
Erick Eloi <65841249+erickeloi@users.noreply.github.com>
2021-02-18 16:47:14 -0300

    Tabuada 3.0

---
+++ Mundo 2/ex067.py
@@ -0,0 +1,13 @@
+# Exercício 66
+# Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário.
+# O programa será interrompido quando o número solicitado for negativo.
+print("Programa Tabuada de números inteiros 'X'")
+while True:
+    tabuada = int(input("Digite um número: "))
+    if tabuada < 0:
+        break
+    else:
+        for contador in range (1, 11):
+            print(f"{tabuada} x {contador} = {tabuada * contador}")
+        print("\nPara parar digite um número negativo!")
+print("Fim do programa Tabuada 3.0")
