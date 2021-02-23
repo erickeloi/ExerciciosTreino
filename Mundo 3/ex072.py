c9ad33f50e349096f527e574dca539eba22e1c23
Erick Eloi <65841249+erickeloi@users.noreply.github.com>
2021-02-23 12:59:51 -0300

    Números escritos por extenso com tuplas

---
+++ Mundo 3/ex072.py
@@ -0,0 +1,6 @@
+# Exercício 72
+# Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso, de zero até vinte.
+# Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
+Numbers = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
+option = int(input("Digite um número ( de 1 a 20 )para saber como escrevê-lo por extenso: "))
+print(Numbers[option])
