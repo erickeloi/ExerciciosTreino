242aaa26f9692791dad3f8d84625090fc0542c47
Erick Eloi <65841249+erickeloi@users.noreply.github.com>
2021-05-20 18:40:39 -0300

    Enviando o ex108 já com o python package

---
+++ Mundo 3/ex108.py
@@ -0,0 +1,34 @@
+# Exercício Python 108: Adapte o código do desafio #107,
+# criando uma função adicional chamada moeda() que consiga mostrar os números como um valor monetário formatado.
+
+from ex108 import moeda
+from time import sleep
+
+while True:
+    print("""
+    [ 1 ] - Dobrar
+    [ 2 ] - Metade
+    [ 3 ] - Aumentar
+    [ 4 ] - Diminuir
+    [ 0 ] - Finalizar programa""")
+    operacao = int(input("\nQual operação você deseja realizar? "))
+    if operacao == 0:
+        break
+    elif operacao == 1:
+        valor = float(input("Digite um valor para ser dobrado: "))
+        print(f"O dobro de {valor} é {moeda.moeda(moeda.dobro(valor))}")
+    elif operacao == 2:
+        valor = float(input("Digite um valor para ser reduzido a metade: "))
+        print(f"A metade de {valor} é {moeda.moeda(moeda.metade(valor))}")
+    elif operacao == 3:
+        valor = float(input("Digite o valor base: "))
+        porcentagem = float(input("Digite a porcentagem de acrescimo: "))
+        print(f"O valor {valor} tendo um acrescimento de {porcentagem}% é: {moeda.moeda(moeda.aumentar(valor, porcentagem))}")
+    elif operacao == 4:
+        valor = float(input("Digite o valor base: "))
+        porcentagem = float(input("Digite a porcentagem de decrescimo: "))
+        print(f"O valor {valor} tendo um decrescimo de {porcentagem}% é: {moeda.moeda(moeda.aumentar(valor, porcentagem))}")
+
+    else:
+        print("Opção inválida!")
+    sleep(4)
---
+++ Mundo 3/ex108/moeda.py
@@ -0,0 +1,20 @@
+def moeda(valor):
+    return f"R${valor}"
+
+
+def metade(numero):
+    return numero / 2
+
+
+def dobro(numero):
+    return numero * 2
+
+
+def aumentar(valor, porcentagem):
+    return valor * (100 + porcentagem)/100
+
+
+def diminuir(valor,porcentagem):
+    return valor * (100 - porcentagem)/100
+
+
