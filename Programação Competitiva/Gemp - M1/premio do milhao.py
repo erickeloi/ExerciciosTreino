2934d028716eddd01f6cf4dbce5561cf5cbb64e2
Erick Eloi <65841249+erickeloi@users.noreply.github.com>
2021-10-10 12:05:38 -0300

    Update premio do milhao.py

--- Programação Competitiva/Gemp - M1/premio do milhao.py
+++ Programação Competitiva/Gemp - M1/premio do milhao.py
@@ -1,10 +1,17 @@
 a = int(input())
 
 tot = int()
-t = list()
+t = int()
 for i in range(0, a):
-    tot += int(input())
-    if tot >= 1000000:
-        t.append(i+1)
 
-print(t[0])
+    # Melhoramento para consumir menos memoria,
+    # Em resumo, quando o total já é maior que 1 milhão, ele para de acumular o total.
+    # Porém, no neps ele da um erro de compilação, visto que o python testado é o 3.7 e esse é o 3.9
+    if tot <= 1000000:
+        tot += int(input())
+        if tot >= 1000000:
+            t = i+1
+    else:
+        b = str(input())
+
+print(t)
