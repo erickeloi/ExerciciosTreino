5b7f9a014f03f8dd87e0e85fd186551c3869d299
Erick Eloi <65841249+erickeloi@users.noreply.github.com>
2022-05-17 20:08:33 -0300

    Update Pilha.py

--- Exercicios_Faculdade/Estrutura_de_dados_I/Pilha.py
+++ Exercicios_Faculdade/Estrutura_de_dados_I/Pilha.py
@@ -11,10 +11,24 @@ class Pilha():
             print(self.elementos[contador], end= ' ')
             contador += 1 
         print()
+    def isPilhaVazia(self):
+        return pilha == []
     def pop(self):
-        aux = self.elementos[-1]
-        self.elementos = self.elementos[:-1]
-        return aux
+        if self.isPilhaVazia():
+            print("Erro! Pilha Vazia")
+        else:
+            aux = self.elementos[-1]
+            self.elementos = self.elementos[:-1]
+            self.topo -= 1
+            return aux
+    def inverter_palavra(self, palavra):
+        palavra = list(palavra)
+        palavra_invertida = ""
+        for letra in palavra[::-1]:
+            palavra_invertida += "" + letra
+
+        return palavra_invertida
+
     def mostrar_palavras_invertidas(self):
         palavras_invertidas_final = []
         palavra = []
@@ -47,8 +61,50 @@ class Pilha():
             print(palavras_invertidas_final[contador], end=' ')
             contador += 1 
         print()
-    def invertido(self):
+
+    def new_mostrar_palavras_invertida(self):
+        frase_completa = self.elementos[0] + " "
+        frase_completa = list(frase_completa)
+        palavra_invertida = ""
+        palavra_aux = ""
+        for elemento in frase_completa:
+            if elemento != " ":
+                palavra_aux += elemento
+            elif elemento == " " or elemento == frase_completa[-1]:
+                palavra_invertida += " " + self.inverter_palavra(palavra_aux)
+                palavra_aux = ""
+
+        return palavra_invertida
+
+    def elementos_invertidos(self):
         return self.elementos[::-1]
+
+
+def inverte_com_pilha(palavra: str):
+    pilha = Pilha()
+    palavra = list(palavra)
+    palavra_invertida = ""
+    # SOLUÇÃO 2
+    for letra in palavra:
+        pilha.insere(letra)
+    
+    contador = pilha.topo - 1
+    while contador != 0:
+        palavra_invertida += pilha.pop()
+        contador -= 1
+    return palavra_invertida
+    # SOLUÇÃO 1
+    #for letra in palavra:
+    #    pilha.insere(letra)
+
+    #contador = pilha.topo -1
+    #while contador != -1:
+    #    palavra_invertida += pilha.elementos[contador]
+    #    contador -= 1
+    
+    #return palavra_invertida
+
+
 
 
 def dec_to_bin(numero: int):
@@ -64,21 +120,28 @@ def dec_to_bin(numero: int):
 
             if numero == 1:
                 restos.insere(1)
-                return restos.invertido()
+                return restos.elementos_invertidos()
             
 
-a = dec_to_bin(10)
-print(a)      
+print(dec_to_bin(10))      
 
 pilha = Pilha()
 
-pilha.insere('ESTE')
-pilha.insere('EXERCICIO')
-pilha.insere('E')
-pilha.insere('MUITO')
-pilha.insere('FACIL')
-print("Mostrando os elementos da pilha... ")
-pilha.imprimir()
-print("Invertendo as Palavras da pilha... ")
-pilha.mostrar_palavras_invertidas()
-print(f"Desempilhando... (pop): {pilha.pop()}")
+pilha.insere("ESTE EXERCICIO E MUITO FACIL")
+#pilha.insere('ESTE')
+#pilha.insere('EXERCICIO')
+#pilha.insere('E')
+#pilha.insere('MUITO')
+#pilha.insere('FACIL')
+#print("Mostrando os elementos da pilha... ")
+#pilha.imprimir()
+#print("Invertendo as Palavras da pilha... ")
+
+#print(pilha.new_mostrar_palavras_invertida())
+
+#print("Invertendo UMA palavra: ")
+#print(pilha.inverter_palavra('ESTE'))
+#print(pilha.invertido())
+#print(f"Desempilhando... (pop): {pilha.pop()}")
+
+print(inverte_com_pilha('exemplo'))
