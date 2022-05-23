bd8400f310847f7c59e369b8a3864f3876a5bcb2
Erick Eloi <65841249+erickeloi@users.noreply.github.com>
2022-05-23 19:55:50 -0300

    Update bom dia.py

--- Exercicios_Faculdade/Programação 1/bom dia.py
+++ Exercicios_Faculdade/Programação 1/bom dia.py
@@ -1,10 +1,17 @@
 #import pandas as pd
+import re
+import string
+import matplotlib.pyplot as plt
 
 #texto1 = open("texto5.txt", "r", encoding="utf8").read()
 texto2 = open("texto37.txt", "r", encoding="utf8").read()
 stopwords = open("stopwords2.txt", "r", encoding="utf8").read().split("\n")
 
-texto2 = texto2.lower().split()
+texto2 = texto2.lower()
+texto2 = texto2.translate(str.maketrans("","", string.punctuation))
+texto2 = texto2.replace("‘","")
+texto2 = texto2.replace("’","")
+texto2 = texto2.split(" ")
 
 clean_text = []
 for word in texto2:
@@ -13,15 +20,17 @@ for word in texto2:
 
 word_count = []
 palavras_unicas = []
+ocorrencias_unicas = []
 #contador = []
 for word in clean_text:
     if word not in palavras_unicas:
         aux = {
-            'word': word,
+            f'word': word,
             'ocorrencies': clean_text.count(word)
         }
         word_count.append(aux)
         palavras_unicas.append(word)
+        ocorrencias_unicas.append(clean_text.count(word))
         #contador.append(clean_text.count(word))
 
   
@@ -31,8 +40,37 @@ word_count = sorted(word_count, key= lambda x: x['ocorrencies'], reverse=True)
 
 #print(word_count)
 contador = 0
-while contador < 10:
-    for single_dict in word_count:
-        for value in single_dict.values():
-            print(value)
-            contador += 1
+#while contador < 10:
+#    for single_dict in word_count:
+#        for value in single_dict.values():
+#            print(value, end = " ")
+#            contador += 1
+#            if contador % 2 == 0:
+#                print()
+
+lista_palavra_freq = []
+for c in range(10):
+    lista_palavra_freq.append(f"{word_count[c]}:{word_count[c]}")
+
+#print(lista_palavra_freq)
+
+#word_count.plot(30,title='Frequency distribution for 30 most common tokens in our text collection')
+
+print(palavras_unicas[:10])
+print(ocorrencias_unicas[:10])
+
+plt.bar(range(len(palavras_unicas[:10])), ocorrencias_unicas[:10])
+
+plt.title("Palavras mais frequentes")
+plt.ylabel("Num. de palavras unicas")
+
+plt.xticks(range(len(ocorrencias_unicas[:10])), ocorrencias_unicas[:10])
+plt.show()
+
+#plt.bar(a, b)
+
+#plt.title("Palavras mais frequentes")
+#plt.ylabel("Num. de palavras unicas")
+
+#plt.xticks(a, b)
+#plt.show()
