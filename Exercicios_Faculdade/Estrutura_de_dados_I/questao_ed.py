8dd2246ab98ae445a0dc077dd09f4d93c4e86d42
Erick Eloi <65841249+erickeloi@users.noreply.github.com>
2022-06-15 22:47:42 -0300

    Update questao_ed.py

--- Exercicios_Faculdade/Estrutura_de_dados_I/questao_ed.py
+++ Exercicios_Faculdade/Estrutura_de_dados_I/questao_ed.py
@@ -1,4 +1,3 @@
-
 class Node():
     """ Nós, que vão encadear a fila"""
     def __init__(self, nome: str, link:str):
@@ -39,15 +38,15 @@ class Fila():
             print("Erro! Pilha Vazia")
 
 
-    def fura_fila(self, nome: str, link: str):
+    def fura_fila(self, nome: str, link: str, aumentar_tamanho: bool = False):
         """ Insere um elemento no começo da fila """
         node = Node(nome, link)
 
         aux_pointer = self.first
         self.first = node
         self.first.next = aux_pointer
-
-        self._size += 1
+        if aumentar_tamanho:
+            self._size += 1
             
     #def __str__(self):
     #""" Mostra a pilha ao usuário ao usar a função print() na classe """
@@ -78,61 +77,98 @@ class Fila():
         print(r)
 
     def busca_site(self, nome: str):
-        index, ponteiro = self._index(nome)
-        if index == None:
+        ponteiro = self.first
+        ponteiro_ant = None
+        contador = 0
+        while ponteiro:
+            if ponteiro.data[0] == nome:
+                break
+            else:
+                contador += 1
+                ponteiro_ant = ponteiro
+                ponteiro = ponteiro.next
+        if contador == 0:
+            print("Site encontrado!")
+            print(f"Link: {ponteiro.data[1]}")
+        elif contador < self._size:
+            print("Site encontrado!")
+            print(f"Link: {ponteiro.data[1]}")
+            # elemento anterior ao desejado = ponteiro_aux
+            data_aux = ponteiro.data
+            ponteiro_ant.next = ponteiro.next
+
+            self.fura_fila(data_aux[0], data_aux[1])
+        elif contador == self._size:
+            print("Site encontrado!")
+            print(f"Link: {ponteiro.data[1]}")
+
+            # elemento anterior ao desejado = ponteiro_aux
+            data_aux = ponteiro.data
+            ponteiro_ant.next = None
+            self.last = ponteiro_ant
+            self.fura_fila(data_aux[0], data_aux[1])
+        else:
             print("Elemento não encontrado")
-        elif index == 0:
-            print("Site encontrado!")
-            print(f"Link: {ponteiro.data[1]}")
-        elif index < self._size:
-            print("Site encontrado!")
-            print(f"Link: {ponteiro.data[1]}")
-            c = 0
-
-            pointeiro_aux = self.first
-            while c < index - 1:
-                c += 1
-                pointeiro_aux = pointeiro_aux.next
-            # elemento anterior ao desejado = ponteiro_aux
-            data_aux = pointeiro_aux.next.data
-            pointeiro_aux.next = pointeiro_aux.next.next
-
-            self.fura_fila(data_aux[0], data_aux[1])
-        elif index == self._size:
-            print("Site encontrado!")
-            print(f"Link: {ponteiro.data[1]}")
-            c = 0
-
-            pointeiro_aux = self.first
-            while c < index - 1:
-                c += 1
-                pointeiro_aux = pointeiro_aux.next
-            # elemento anterior ao desejado = ponteiro_aux
-            data_aux = pointeiro_aux.next.data
-            pointeiro_aux.next = None
-            pointeiro_aux = self.last
-            self.fura_fila(data_aux[0], data_aux[1])
-
 
     def _index(self, nome: str):
-        """Dado um nome, procura e retorna em qual index esse nome está alocado"""
+        """Dado um nome, procura e retorna em qual index esse nome está alocado, também retorna o ponteiro e o ponteiro do antecessor"""
         pointer = self.first
-        
+        pointer_ant = None
         contador = 0
         while pointer:
             if pointer.data[0] == nome:
-                return contador, pointer
+                return contador, pointer, pointer_ant
             else:
-                pointer = pointer.next
                 contador += 1
+                pointer_ant = pointer
+                pointer = pointer.next
+                
 
-        print("Elemento não encontrado na lista!")
-        return None, None
+
+    def remove_site(self, nome: str):
+        ponteiro = self.first
+        ponteiro_ant = None
+
+        contador = 0
+        while ponteiro:
+            if ponteiro.data[0] == nome:
+                break
+            else:
+                contador += 1
+                if contador == self._size:
+                    contador = -1
+                    break
+                ponteiro_ant = ponteiro
+                ponteiro = ponteiro.next
+
+        if contador == -1:
+            print("Elemento não encontrado")
+        elif contador == 0:
+            print(f"Removendo o site {ponteiro.data[0]}...")
+            self.first = self.first.next
+            self._size -= 1
+        elif contador < self._size:
+            print(f"Removendo o site {ponteiro.data[0]}...")
+            # elemento anterior ao desejado = ponteiro_aux
+            ponteiro_ant.next = ponteiro.next
+            self._size -= 1
+        elif contador == self._size:
+            print(f"Removendo o site {ponteiro.data[0]}...")
+
+            # elemento anterior ao desejado = ponteiro_aux
+            ponteiro_ant.next = None
+            self.last = ponteiro_ant
+            self._size -= 1
+        else:
+            print("Elemento não encontrado na lista!")
 
     def __len__(self):
         """ Mostra o tamanho da fila ao usar a função len() na fila """
         return self._size
 
+def quebra_linha():
+    print("---")
+    print("\n")
 bd = Fila()
 bd.insere_site("facebook", "https://www.facebook.com")
 bd.insere_site("instagram", "https://www.instagram.com")
@@ -141,25 +177,40 @@ bd.insere_site("netflix", "https://www.netflix.com/")
 bd.insere_site("amazon", "https://www.amazon.com.br")
 bd.insere_site("pudim", "http://www.pudim.com.br")
 
-print("Mostrando a o banco de dados (fila)...")
+print("Mostrando o banco de dados (fila)...")
 bd.mostra_fila()
+quebra_linha()
 
-print("---")
 print("Buscando por Pudim...")
 bd.busca_site("pudim")
-print("---")
-bd.mostra_fila()
+quebra_linha()
 
-print("---")
+bd.mostra_fila()
+quebra_linha()
+
 print("Buscando por Twitter...")
 bd.busca_site("twitter")
-print("---")
+quebra_linha()
 
 bd.mostra_fila()
+quebra_linha()
 
-print("---")
 print("Buscando por Amazon...")
 bd.busca_site("amazon")
-print("---")
+quebra_linha()
 
 bd.mostra_fila()
+quebra_linha()
+
+print(" Removendo o site Twitter ")
+bd.remove_site("twitter")
+
+bd.mostra_fila()
+quebra_linha()
+
+print(" Tentando Remover o site Twitter novamente (ele não está mais no banco de dados )")
+bd.remove_site("twitter")
+quebra_linha()
+
+bd.mostra_fila()
+quebra_linha()
